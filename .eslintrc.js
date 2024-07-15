1471fea74ab397e346218d7fb98776e8ff8d43bf
TracyMuso <musongoletracy@gmail.com>
2024-07-15 11:22:58 +0200

    configure prettier and husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,14 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'love', 'next/core-web-vitals'],
+  extends: [
+    'plugin:react/recommended',
+    'love',
+    'next/core-web-vitals',
+    'prettier',
+  ],
   overrides: [],
-  plugins: ['react'],
+  plugins: ['react', 'prettier'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
@@ -16,7 +21,7 @@ module.exports = {
     },
   },
   rules: {
-    "@typescript-eslint/consistent-type-definitions": ["off"],
-    "@typescript-eslint/explicit-function-return-type": "off",
+    '@typescript-eslint/consistent-type-definitions': ['off'],
+    '@typescript-eslint/explicit-function-return-type': 'off',
   },
 }
