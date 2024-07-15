0127bf34ade75439f35458c46f40355fb1fe5bd7
TracyMuso <musongoletracy@gmail.com>
2024-07-15 11:14:37 +0200

    configure typescript eslint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'love', 'next/core-web-vitals'],
+  overrides: [],
+  plugins: ['react'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: true,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    "@typescript-eslint/consistent-type-definitions": ["off"],
+    "@typescript-eslint/explicit-function-return-type": "off",
+  },
+}
