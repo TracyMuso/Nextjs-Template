14571663cb3d76962e9428119a20e45f039764d8
TracyMuso <musongoletracy@gmail.com>
2024-07-15 18:17:36 +0200

    update babel and storybook config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,17 +5,27 @@ const config: StorybookConfig = {
     '../stories/**/*.mdx',
     '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
+
   addons: [
     '@storybook/addon-onboarding',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@chromatic-com/storybook',
     '@storybook/addon-interactions',
+    '@storybook/addon-mdx-gfm',
   ],
+
   framework: {
     name: '@storybook/nextjs',
     options: {},
   },
+
   staticDirs: ['../public'],
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
+  },
 }
 export default config
--- app/page.tsx
+++ app/page.tsx
@@ -7,3 +7,18 @@ export default function Home() {
     </div>
   )
 }
+
+/* 
+npm install --save-dev \
+  storybook-css-modules \
+  @storybook/preset-scss \
+  @storybook/react \
+  @storybook/test\
+  @storybook/addon-essentials \
+  @storybook/addon-interactions \
+  @storybook/addon-links \
+  @storybook/addon-postcss \
+  @storybook/addon-styling-webpack \
+  @storybook/addon-themes \
+  @storybook/blocks
+*/
